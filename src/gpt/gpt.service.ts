e3c8ff399ef428bcfb2d250d5c77a0730658d7cf
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-05-07 11:10:07 -0500

    orthography backen ok

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -3,8 +3,9 @@ import {
   orthographyCheckUseCase,
   prosConsDicusserStreamUseCase,
   prosConsDicusserUseCase,
+  translateUseCase,
 } from './use-cases';
-import { OrthographyDto, ProsConsDiscusserDto } from './dtos';
+import { OrthographyDto, ProsConsDiscusserDto, TranslateDto } from './dtos';
 import OpenAI from 'openai';
 
 @Injectable()
@@ -24,5 +25,8 @@ export class GptService {
   }
   async prosConsDicusserStream({ prompt }: ProsConsDiscusserDto) {
     return await prosConsDicusserStreamUseCase(this.openai, { prompt });
+  }
+  async translateText({ prompt, lang }: TranslateDto) {
+    return await translateUseCase(this.openai, { prompt, lang });
   }
 }
---
+++ src/gpt/use-cases/translate.use-case.ts
@@ -0,0 +1,31 @@
+import OpenAI from 'openai';
+
+interface Options {
+  prompt: string;
+  lang: string;
+}
+
+export const translateUseCase = async (
+  openai: OpenAI,
+  { prompt, lang }: Options,
+) => {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o',
+    messages: [
+      {
+        role: 'system',
+        content: `
+        Traduce el siguiente texto al idioma ${lang}:${prompt}
+        `,
+      },
+    ],
+    temperature: 0.2,
+    // max_tokens: 500,
+  });
+
+  console.log(response);
+
+  return {
+    message: response.choices[0].message.content,
+  };
+};
